325cc96ce94805bb3d3d90f2f998230c6dfe3569
Akshansh Kakkar <akshanshkakkar92@gmail.com>
2026-02-11 08:29:33 +0530

    final commit

--- src/Components/Bio.jsx
+++ src/Components/Bio.jsx
@@ -1,18 +1,68 @@
+const Bio = [
+  {
+    Id: 1,
+    Name: "Akshansh kakkar",
+    Role: "Product Manager",
+    Skill: [
+      "Design",
+      "React",
+      "TypeScript",
+      "Figma",
+      "Prototyping",
+      "Accessibility",
+    ],
+    Img: "/assets/akshansh.png",
+    Description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi minus quam et accusamus eius at enim tempore, autem ipsa totam nostrum eum neque, quod velit dolorem error eligendi illo beatae?",
+  },
+  {
+    Id: 2,
+    Name: "Person 2",
+    Role: "Product Manager",
+    Skill: [
+      "Design",
+      "React",
+      "TypeScript",
+      "Figma",
+      "Prototyping",
+      "Accessibility",
+    ],
+    Img: "/assets/photo2.jpeg",
+    Description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi minus quam et accusamus eius at enim tempore, autem ipsa totam nostrum eum neque, quod velit dolorem error eligendi illo beatae?",
+  },
+  {
+    Id: 3,
+    Name: "Person 3",
+    Role: "Product Manager",
+    Skill: [
+      "Design",
+      "React",
+      "TypeScript",
+      "Figma",
+      "Prototyping",
+      "Accessibility",
+    ],
+    Img: "/assets/photo1.jpg",
+    Description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi minus quam et accusamus eius at enim tempore, autem ipsa totam nostrum eum neque, quod velit dolorem error eligendi illo beatae?",
+  },
+  {
+    Id: 4,
+    Name: "Person 4",
+    Role: "Product Manager",
+    Skill: [
+      "Design",
+      "React",
+      "TypeScript",
+      "Figma",
+      "Prototyping",
+      "Accessibility",
+    ],
+    Img: "/assets/person4.jpg",
+    Description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi minus quam et accusamus eius at enim tempore, autem ipsa totam nostrum eum neque, quod velit dolorem error eligendi illo beatae?",
+  },
+];
 
-const Bio=[
-    {
-     Id: 1,
-   Name: "Akshansh kakkar",
-   Role: "Product Manager",
-   Skill: [
-    "Design",
-    "React",
-    "TypeScript",
-    "Figma",
-    "Prototyping",
-    "Accessibility",],
-   Img: "/assets/akshansh.png",
-   Description :"Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi minus quam et accusamus eius at enim tempore, autem ipsa totam nostrum eum neque, quod velit dolorem error eligendi illo beatae?",
-  }]
-
-export default Bio
+export default Bio;
--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -4,11 +4,23 @@ const Card = () => {
   const [Info, setInfo] = useState(Bio);
   const [count, setCount] = useState(0);
   const [theme, setTheme] = useState(false);
+  const [cardIndex, setCardIndex] = useState(0);
   const Toggletheme = () => {
     setTheme((prev) => !prev);
   };
   const LikeCount = () => {
     setCount(count + 1);
+  };
+  const mail = () => {
+    alert("Stop touching me!");
+  };
+  const User = Info[cardIndex];
+
+  const PreviousCard = () => {
+    setCardIndex((prev) => (prev - 1 + Info.length) % Info.length);
+  };
+  const NextCard = () => {
+    setCardIndex((prev) => (prev + 1) % Info.length);
   };
   return (
     <div
@@ -30,29 +42,28 @@ const Card = () => {
         </div>
       </div>
       <div className="flex justify-center">
-        {Info &&
-          Info.map((user) => (
+        {User &&
             <div
-              key={user.Id}
+              key={User.Id}
               className={`w-[500px] h-[500px] p-6  rounded-xl ${theme ? "bg-gray-500" : "bg-white"}`}
             >
               <div className="flex items-center gap-2">
                 <img
-                  src={user.Img}
+                  src={User.Img}
                   style={{ width: 90, height: 90, margin: 5 }}
                   className="full-rounded"
-                  alt={user.Name}
+                  alt={User.Name}
                 />
                 <div className="flex flex-col text-left">
                   <p
                     className={`text-2xl font-semibold  tracking-widest ${theme ? "text-white " : "text-black"}`}
                   >
-                    {user.Name}
+                    {User.Name}
                   </p>
                   <p
                     className={`text-md font-normal font-sans ${theme ? "text-gray-100" : "text-gray-600"}`}
                   >
-                    {user.Role}
+                    {User.Role}
                   </p>
                 </div>
               </div>
@@ -60,7 +71,7 @@ const Card = () => {
                 <div
                   className={`flex justify-center ${theme ? "text-white" : "text-black"}`}
                 >
-                  {user.Description}
+                  {User.Description}
                 </div>
               </div>
               <div
@@ -69,7 +80,7 @@ const Card = () => {
                 Skills
               </div>
               <div className="flex flex-wrap gap-2  items-center justify-between pt-2 text-md  mx-2">
-                {user.Skill.map((Skill, Index) => (
+                {User.Skill.map((Skill, Index) => (
                   <span
                     className={`py-1  px-2 flex  items-center rounded-2xl ${theme ? "bg-white" : "bg-blue-100"}`}
                     key={Index}
@@ -87,24 +98,36 @@ const Card = () => {
                       alt="dark mode"
                     />
                   </span>
-                  <span className={` ${theme ?  "text-white": "text-black"}`}>{theme ? "dark mode" : "light mode"}</span>
+                  <span className={` ${theme ? "text-white" : "text-black"}`}>
+                    {theme ? "dark mode" : "light mode"}
+                  </span>
                 </div>
                 <div className="flex gap-4 items-center">
-                  <span>
+                  <span onClick={PreviousCard}>
                     <img
-                      src={theme ? "assets/left-white.svg" : "assets/left-black.svg"}
+                      src={
+                        theme
+                          ? "assets/left-white.svg"
+                          : "assets/left-black.svg"
+                      }
                       style={{ width: 20 }}
                       alt="previous"
                     />
                   </span>
-                  <span>
+                  <span onClick={NextCard}>
                     <img
-                      src={theme ? "assets/right-white.svg" : "assets/right-black.svg"}
+                      src={
+                        theme
+                          ? "assets/right-white.svg"
+                          : "assets/right-black.svg"
+                      }
                       style={{ width: 20 }}
                       alt="next"
                     />
                   </span>
-                  <span className={`${theme ? "text-white" : "text-black"}`}>1/4</span>
+                  <span className={`${theme ? "text-white" : "text-black"}`}>
+                    {cardIndex + 1 }/{ Info.length}
+                  </span>
                 </div>
                 <div
                   className="gap-2 flex items-center text-center rounded-xl"
@@ -112,26 +135,41 @@ const Card = () => {
                 >
                   <span>
                     <img
-                      src={theme ? "assets/unfille-heart-white.svg" : "assets/unfille-heart.svg"}
+                      src={
+                        theme
+                          ? "assets/unfille-heart-white.svg"
+                          : "assets/unfille-heart.svg"
+                      }
                       style={{ width: 30 }}
                       alt="like"
                     />
                   </span>
-                  <span className={`${theme ? "text-white" : "text-black"}`}>{count}</span>
+                  <span className={`${theme ? "text-white" : "text-black"}`}>
+                    {count}
+                  </span>
                 </div>
-                <div className="gap-2 flex items-center text-center bg-blue-400 py-1 px-3 rounded-xl">
+                <div
+                  className="gap-2 flex items-center text-center bg-blue-400 py-1 px-3 rounded-xl cursor-pointer"
+                  onClick={mail}
+                >
                   <span>
                     <img
-                      src={theme ? "assets/white-mail.svg": "assets/black-mail.svg"}
+                      src={
+                        theme
+                          ? "assets/white-mail.svg"
+                          : "assets/black-mail.svg"
+                      }
                       style={{ width: 30 }}
                       alt="dark mode"
                     />
                   </span>
-                  <span className={`${theme ? "text-white" : "text-black"}`}>mail</span>
+                  <span className={`${theme ? "text-white" : "text-black"} `}>
+                    mail
+                  </span>
                 </div>
               </div>
             </div>
-          ))}
+          }
       </div>
     </div>
   );
